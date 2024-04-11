799baebe2cebbaea03c24978fadecbe4a75c5385
Sevan <sevanbro7@gmail.com>
2024-04-11 15:15:39 -0500

    Clearing out main css

--- src/App.js
+++ src/App.js
@@ -12,10 +12,8 @@ function App() {
 
   return (
     <div className="App">
-      {/* Dedicated div for p5 sketch, ensuring it's behind all other content */}
       <div ref={sketchRef} style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%' }}></div>
       
-      {/* The rest of your app content goes here, styled to be on top of the background */}
       <header className="App-header">
       </header>
     </div>
