42c07d5d1fa1b7175fa222000308040ffbeb9304
Sevan <sevanbro7@gmail.com>
2024-04-11 15:09:04 -0500

    clearing out main js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import p5 from 'p5';
-import logo from './logo.svg';
 import homeAnimation from './homeAnimation';
 import './App.css';
 
 function App() {
-  const [numBlogPosts, setNumBlogPosts] = useState(0);
-
-  useEffect(() => {
-    fetch(`${process.env.REACT_APP_API_URL}/api/blogposts/`)
-      .then(response => response.json())
-      .then(data => {
-        setNumBlogPosts(data.length);
-      })
-      .catch(error => console.error("There was an error fetching the blog posts:", error));
-  }, []);
 
   const sketchRef = useRef();
   useEffect(() => {
@@ -28,20 +17,7 @@ function App() {
       
       {/* The rest of your app content goes here, styled to be on top of the background */}
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          TEST - Hello World! There are {numBlogPosts} blog posts.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
-      <p>TEST TEST</p>
     </div>
   );
 }
