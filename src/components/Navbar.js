69724bd87aef82f7100adad30c365db9017b6c76
Sevan <sevanbro7@gmail.com>
2024-05-16 21:58:01 -0500

    Cleaned up  navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,32 +10,31 @@ function Navbar() {
   const location = useLocation();
 
   return (
-    <nav className="navbar">
-      <div className="logoAndDropdown">
-        <Link to="/" className="logo">
-          <img src={logo} alt="Logo" />
-          <span className="site-name">SevanB.net</span>
-        </Link>
-
-        <div className="spacer"></div>
-
-        <div className={`menu-icon ${isNavExpanded ? 'open' : ''}`} onClick={() => setIsNavExpanded(!isNavExpanded)}>
-          <div></div><div></div><div></div>
+    <div className="navbarContainer">
+      <nav className="navbar">
+        <div className="logoAndDropdown">
+          <Link to="/" className="logo">
+            <img src={logo} alt="Logo" />
+            <span className="site-name">SevanB.net</span>
+          </Link>
+          <div className="spacer"></div>
+          <div className={`menu-icon ${isNavExpanded ? 'open' : ''}`} onClick={() => setIsNavExpanded(!isNavExpanded)}>
+            <div></div><div></div><div></div>
+          </div>
         </div>
-      </div>
-      
-      <ul className={`nav-links ${isNavExpanded ? 'open' : ''}`}>
-        <li><Link to="/" onClick={() => setIsNavExpanded(false)} className={location.pathname === "/" ? "active" : ""}>Home</Link></li>
-        <li><Link to="/projects" onClick={() => setIsNavExpanded(false)} className={location.pathname === "/projects" ? "active" : ""}>Projects</Link></li>
-        <li><Link to="/blog" onClick={() => setIsNavExpanded(false)} className={location.pathname === "/blog" ? "active" : ""}>Blog</Link></li>
-        <li><Link to="/about" onClick={() => setIsNavExpanded(false)} className={location.pathname === "/about" ? "active" : ""}>About Me</Link></li>
-      </ul>
+        <ul className={`nav-links ${isNavExpanded ? 'open' : ''}`}>
+          <li><Link to="/" onClick={() => setIsNavExpanded(false)} className={location.pathname === "/" ? "active" : ""}>Home</Link></li>
+          <li><Link to="/projects" onClick={() => setIsNavExpanded(false)} className={location.pathname === "/projects" ? "active" : ""}>Projects</Link></li>
+          <li><Link to="/blog" onClick={() => setIsNavExpanded(false)} className={location.pathname === "/blog" ? "active" : ""}>Blog</Link></li>
+          <li><Link to="/about" onClick={() => setIsNavExpanded(false)} className={location.pathname === "/about" ? "active" : ""}>About Me</Link></li>
+        </ul>
+        <div className={`social-links ${isNavExpanded ? 'open' : ''}`}>
+          <Link to="https://github.com/SevanBrodjian" target="_blank"><img src={gitLogo} alt="GitHub" /></Link>
+          <Link to="https://www.linkedin.com/in/sevan-b/" target="_blank"><img src={liLogo} alt="LinkedIn" /></Link>
+        </div>
+      </nav>
+    </div>
 
-      <div className={`social-links ${isNavExpanded ? 'open' : ''}`}>
-        <Link to="https://github.com/SevanBrodjian" target="_blank"><img src={gitLogo} alt="GitHub" /></Link>
-        <Link to="https://www.linkedin.com/in/sevan-b/" target="_blank"><img src={liLogo} alt="LinkedIn" /></Link>
-      </div>
-    </nav>
   );
 }
 
