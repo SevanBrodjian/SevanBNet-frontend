155459cd3aa9735d22fcef28e27de35c664d7de7
Sevan <sevanbro7@gmail.com>
2024-06-04 15:37:10 -0500

    Added Navigate import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
 import Blog from "./components/Blog";
